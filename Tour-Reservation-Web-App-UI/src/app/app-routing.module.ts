87468d2b69a589d6c0bb61151f976f42a3660bd6
Elefteria07 <kuruzeynepsude07@gmail.com>
2023-07-31 09:31:28 +0300

    Homepage & register added

--- Tour-Reservation-Web-App-UI/src/app/app-routing.module.ts
+++ Tour-Reservation-Web-App-UI/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'register',component : RegisterpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Tour-Reservation-Web-App-UI/src/app/app.module.ts
+++ Tour-Reservation-Web-App-UI/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RegisterpageComponent } from './components/registerpage/registerpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginpageComponent
+    LoginpageComponent,
+    HomepageComponent,
+    RegisterpageComponent
   ],
   imports: [
     BrowserModule,
