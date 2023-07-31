85634943494230ab7d5d5d65db8c701d7509cc0a
Mustafa Berk Alpay <kisacaberk@gmail.com>
2023-07-31 13:11:52 +0300

    ogin register frontend v0.01

--- Tour-Reservation-Web-App-UI/src/app/app-routing.module.ts
+++ Tour-Reservation-Web-App-UI/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterpageComponent } from './components/registerpage/registerpage.component';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
 
 const routes: Routes = [
-  {path :'register',component : RegisterpageComponent}
+  {path :'register',component : RegisterpageComponent},
+  {path :'login',component : LoginpageComponent}
 ];
 
 @NgModule({
